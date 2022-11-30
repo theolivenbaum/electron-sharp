c807a3d127b156526841357e700f66ed4732da6a
theolivenbaum <rafael@curiosity.ai>
2022-11-30 13:56:26 +0100

    Release 22.0.0

--- ElectronNET.CLI/Commands/BuildCommand.cs
+++ ElectronNET.CLI/Commands/BuildCommand.cs
@@ -10,7 +10,7 @@ namespace ElectronNET.CLI.Commands
 {
     public class BuildCommand : ICommand
     {
-        private const string _defaultElectronVersion = "21.3.1";
+        private const string _defaultElectronVersion = "22.0.0";
 
         public const string COMMAND_NAME = "build";
         public const string COMMAND_DESCRIPTION = "Build your Electron Application.";
