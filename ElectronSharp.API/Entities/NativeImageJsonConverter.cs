4c99d221e584d230a5a136968d92b95cf5c431ce
Pius Friesch <pius@curiosity.ai>
2023-08-08 18:43:21 +0200

    improve img clipboard handling

--- ElectronSharp.API/Entities/NativeImageJsonConverter.cs
+++ ElectronSharp.API/Entities/NativeImageJsonConverter.cs
@@ -4,6 +4,7 @@ using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Png;
 using SixLabors.ImageSharp.Processing;
 using System.IO;
+using System.Linq;
 using Newtonsoft.Json;
 
 namespace ElectronSharp.API.Entities
@@ -24,16 +25,19 @@ namespace ElectronSharp.API.Entities
             if (reader.TokenType == JsonToken.Null) return null;
 
             var dict = serializer.Deserialize<Dictionary<string, string>>(reader);
+
             var newDictionary = new Dictionary<float, Image>();
+
             foreach (var item in dict)
             {
-                if (float.TryParse(item.Key, out var size))
+                if (float.TryParse(item.Key, out var size) && !string.IsNullOrWhiteSpace(item.Value))
                 {
                     var bytes = Convert.FromBase64String(item.Value);
+
                     newDictionary.Add(size, Image.Load(new MemoryStream(bytes)));
                 }
             }
-            return new NativeImage(newDictionary);
+            return newDictionary.Any() ? new NativeImage(newDictionary) : null;
         }
 
         public override bool CanConvert(Type objectType) => objectType == typeof(NativeImage);
