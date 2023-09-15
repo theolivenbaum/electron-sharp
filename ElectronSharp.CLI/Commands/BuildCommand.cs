f24148b8a2c3d33e0729315c9281a4e6a7f05dc0
theolivenbaum <rafael@curiosity.ai>
2023-09-15 14:18:32 +0200

    Release 26.2.1

--- ElectronSharp.CLI/Commands/BuildCommand.cs
+++ ElectronSharp.CLI/Commands/BuildCommand.cs
@@ -10,7 +10,7 @@ namespace ElectronSharp.CLI.Commands
 {
     public class BuildCommand : ICommand
     {
-        private const string _defaultElectronVersion = "26.2.0";
+        private const string _defaultElectronVersion = "26.2.1";
 
         public const string COMMAND_NAME = "build";
         public const string COMMAND_DESCRIPTION = "Build your Electron Application.";
